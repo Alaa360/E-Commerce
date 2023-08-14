3038e4670a5866b53bbe8b7e6484c31f2a7d3253
Alaa360 <go_4.dance@live.com>
2023-08-14 18:36:21 +0300

    create Rating component

--- appearance/src/screens/HomeScreen.js
+++ appearance/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
-//import data from "../data";
 import { useEffect, useReducer } from "react";
 import axios from "axios";
 import logger from "use-reducer-logger";
+import Product from "../components/product";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -46,20 +47,13 @@ function HomeScreen() {
           ) : error ? (
             <div>{error}</div>
           ) : (
-            products.map((product) => (
-              <div className="product" key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className="product-info">
-                  <p>{product.name}</p>
-                  <p>
-                    <strong>{product.price} EGP</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))
+            <Row>
+              {products.map((product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                  <Product product={product}></Product>
+                </Col>
+              ))}
+            </Row>
           )}
         </div>
       </div>
